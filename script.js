94b53524696d2e6406d5ed71ab6d4bd40f061976
noworryravi <noworryravi@gmail.com>
2024-02-10 15:29:13 +0530

    add Theme Changer project

---
+++ script.js
@@ -0,0 +1,35 @@
+const red = document.querySelector("#red");
+const yellow = document.querySelector("#yellow");
+const cornflowerblue = document.querySelector("#cornflowerblue");
+const green = document.querySelector("#green");
+const pink = document.querySelector("#pink");
+const orange = document.querySelector("#orange");
+const white = document.querySelector("#white");
+const black = document.querySelector("#black");
+const container = document.querySelector(".container");
+
+red.addEventListener("click", function(){
+    container.style.backgroundColor = "red"
+})
+yellow.addEventListener("click", function(){
+    container.style.backgroundColor = "yellow"
+})
+cornflowerblue.addEventListener("click", function(){
+    container.style.backgroundColor = "cornflowerblue"
+})
+green.addEventListener("click", function(){
+    container.style.backgroundColor = "green"
+})
+violet.addEventListener("click", function(){
+    container.style.backgroundColor = "violet"
+})
+orange.addEventListener("click", function(){
+    container.style.backgroundColor = "orange"
+})
+white.addEventListener("click", function(){
+    container.style.backgroundColor = "white"
+})
+black.addEventListener("click", function(){
+    container.style.backgroundColor = "black"
+})
+
